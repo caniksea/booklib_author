e59e40e145940a12414ac1a7ad3fd230e795a8fb
minuszero <pcijideani@gmail.com>
2021-04-22 10:20:11 +0200

    Implemented Author domain using Builder pattern, and implemented the factory for Author

--- src/main/java/com/caniksea/adp3/practical/booklib/authormodule/domain/generic/Author.java
+++ src/main/java/com/caniksea/adp3/practical/booklib/authormodule/domain/generic/Author.java
@@ -11,4 +11,61 @@ package com.caniksea.adp3.practical.booklib.authormodule.domain.generic;
 public class Author {
 
     private String id, firstName, lastName; // DO NOT MODIFY THIS LINE.
+
+    private Author(Builder builder) {
+        this.id = builder.id;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public static class Builder {
+        private String id, firstName, lastName;
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder copy(Author author) {
+            this.id = author.id;
+            this.firstName = author.firstName;
+            this.lastName = author.lastName;
+            return this;
+        }
+
+        public Author build() {
+            return new Author(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
